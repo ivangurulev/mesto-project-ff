ada2a867461c84ac4b07afa6a341ca0d3b74ca8c
Ivan Gurulev <ivangurulev6043@gmail.com>
2024-11-07 15:45:03 +0300

    Первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function addCard(name, link, cardDelete) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardList = document.querySelector(".places__list");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  deleteButton.onclick = function () {
+    cardDelete(cardElement);
+  };
+
+  cardList.append(cardElement);
+}
+
+function cardDelete(cardElement) {
+  cardElement.remove();
+}
+
+initialCards.forEach((item) => {
+  addCard(item.name, item.link, cardDelete);
+});
